fix guides 3 and 4 fix numerical_gradient and add test

edunn/models/activations.py
@@ -1,4 +1,4 @@
-from edunn.model import ModelWithParameters, Cache, ModelWithoutParameters
+from edunn.model import ModelWithParameters, ModelWithoutParameters
 import numpy as np
 import math
 
@@ -27,20 +27,22 @@ class AddConstant(ModelWithParameters):
     def forward(self, x: np.ndarray):
         """
         :param x: input vector/matrix
-        :return: x + a constant value, stored in self.value
+        :return: `x + a`, constant value, stored in `self.value`
         """
-        y = np.zeros_like(x)
-        ### YOUR IMPLEMENTATION START  ###
 
+        ### YOUR IMPLEMENTATION START  ###
+        # default: y = np.zeros_like(x)
         y = x + self.value
         ### YOUR IMPLEMENTATION END  ###
+
         return y
 
     def backward(self, dE_dy: np.ndarray):
-        dE_dx = np.zeros_like(dE_dy)
         ### YOUR IMPLEMENTATION START  ###
+        # default: dE_dx = np.zeros_like(dE_dy)
         dE_dx = dE_dy
         ### YOUR IMPLEMENTATION END  ###
+
         dE_dp = {}  # no parameters, no derivatives
         return dE_dx, dE_dp
 
@@ -58,18 +60,19 @@ class MultiplyConstant(ModelWithParameters):
     def forward(self, x: np.ndarray):
         """
         :param x: input vector/matrix
-        :return: x * a constant value, stored in self.value
+        :return: `x * a`, constant value, stored in `self.value`
         """
-        y = np.zeros_like(x)
+
         ### YOUR IMPLEMENTATION START  ###
+        # default: y = np.zeros_like(x)
         y = x * self.value
         ### YOUR IMPLEMENTATION END  ###
+
         return y
 
     def backward(self, dE_dy: np.ndarray):
-        dE_dx = np.zeros_like(dE_dy)
-
         ### YOUR IMPLEMENTATION START  ###
+        # default: dE_dx = np.zeros_like(dE_dy)
         dE_dx = dE_dy * self.value
         ### YOUR IMPLEMENTATION END  ###
 

edunn/utils/check_gradient.py
@@ -7,19 +7,19 @@ from edunn.utils.numerical_gradient import numerical_gradient
 from colorama import Fore, Back, Style
 
 
-def common_layer(l: nn.ModelWithParameters, x_shape: Tuple, samples: int = 1, tolerance=1e-7, break_on_error=True):
+def common_layer(layer: nn.ModelWithParameters, x_shape: Tuple, samples: int = 1, tolerance=1e-7, break_on_error=True):
     """
     Generates random inputs for samples of x, and random parameters to check
     forward: calculates numerical gradient
     backward: calculates analytical gradient
     and compares them
     """
-    f, df, parameter_shapes = common_layer_to_function(l)
+    f, df, parameter_shapes = common_layer_to_function(layer)
     shapes = {**parameter_shapes, "x": x_shape}
     input_generators = lambda: {k: np.random.normal(0, 1, shape) for k, shape in shapes.items()}
     y = f(input_generators())
     dE_dy_generator = lambda: np.random.normal(0, 1, y.shape)
-    common_layer_random_sample(l, f, df, input_generators, dE_dy_generator, samples=samples, tolerance=tolerance,
+    common_layer_random_sample(layer, f, df, input_generators, dE_dy_generator, samples=samples, tolerance=tolerance,
                                break_on_error=break_on_error)
 
 
@@ -84,10 +84,10 @@ def binary_cross_entropy_labels(l: nn.BinaryCrossEntropy, batch_size: int, sampl
                                tolerance=tolerance, break_on_error=break_on_error)
 
 
-def common_layer_random_sample(l: nn.Model, f, df, input_generator, dE_dy_generator, samples: int = 1, tolerance=1e-7,
+def common_layer_random_sample(layer: nn.Model, f, df, input_generator, dE_dy_generator, samples: int = 1, tolerance=1e-7,
                                break_on_error=True):
     checks, errors = 0, 0
-    print(f"{Back.LIGHTBLUE_EX}{Fore.BLACK}{l.name} layer:{Style.RESET_ALL}")
+    print(f"{Back.LIGHTBLUE_EX}{Fore.BLACK}{layer.name} layer:{Style.RESET_ALL}")
 
     for i in range(samples):
         inputs = input_generator()

edunn/utils/numerical_gradient.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def numerical_gradient(f, x: np.ndarray, dE_dy: np.ndarray = None, h=1e-5):
+def numerical_gradient(f, x: np.ndarray, dE_dy: np.ndarray = None, h=1e-5) -> np.ndarray:
     """
     Calculates the numerical gradient of E wrt x
     E is assumed to be a scalar, so that dE_dy has size equal to y
@@ -18,17 +18,17 @@ def numerical_gradient(f, x: np.ndarray, dE_dy: np.ndarray = None, h=1e-5):
     h2 = 2 * h
     dE_dx = np.zeros_like(x)
     for i in indices_generator(x):
-        oldval = x[i]
+        x_value = x[i]
         # increment by h
-        x[i] = oldval + h
+        x[i] = x_value + h
         # evaluate f(x + h)
         fxph = f(x)
-        # decrement by h2
-        x[i] = oldval - h2
+        # decrement by h
+        x[i] = x_value - h
         # evaluate f(x - h)
         fxmh = f(x)
         # reset
-        x[i] = oldval
+        x[i] = x_value
 
         dy_dxi = (fxph - fxmh) / h2
 