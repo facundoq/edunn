e3935dd830588691fc2d5abbe7550f914be0566e
Sergio Marchio <sergiomarchio@gmail.com>
2024-09-27 00:01:05 -0300

    remove MultiplyConstant

--- edunn/models/activations.py
+++ edunn/models/activations.py
@@ -51,40 +51,6 @@ class AddConstant(Model):
         return dE_dx, dE_dp
 
 
-class MultiplyConstant(Model):
-    """
-    A layer that multiplies by a constant
-    This layer has NO parameters
-    """
-
-    def __init__(self, value: float, name=None):
-        super().__init__(name=name)
-        self.value = value
-
-    def forward(self, x: np.ndarray):
-        """
-        :param x: input vector/matrix
-        :return: `x * a`, constant value, stored in `self.value`
-        """
-
-        """ YOUR IMPLEMENTATION START """
-        # default: y = np.zeros_like(x)
-        y = x * self.value
-        """ YOUR IMPLEMENTATION END """
-
-        return y
-
-    def backward(self, dE_dy: np.ndarray):
-        """ YOUR IMPLEMENTATION START """
-        # default: dE_dx = np.zeros_like(dE_dy)
-        dE_dx = dE_dy * self.value
-        """ YOUR IMPLEMENTATION END """
-
-        # no parameters, no derivatives
-        dE_dp = {}
-        return dE_dx, dE_dp
-
-
 class ReLU(Model):
 
     def forward(self, x: np.ndarray):
